7231482a09dc708d29bae0b7550afad0e39d427b
Muxammed Safarov <muxammed@Muxammeds-MacBook-Pro.local>
2023-10-12 14:05:25 +0400

    Mock data added

---
+++ recipe-app/src/Data/recipe.js
@@ -0,0 +1,67 @@
+const recipes = [
+  {
+    id: 1,
+    title: "Chicken & mushroom Hotpot",
+    description:
+      "Heat oven to 200C/180C fan/gas 6. Put the butter in a medium-size saucepan and place over a medium heat. Add the onion and leave to cook for 5 mins, stirring occasionally. Add the mushrooms to the saucepan with the onions. Once the onion and mushrooms are almost cooked, stir in the flour – this will make a thick paste called a roux.",
+    image: "https://www.themealdb.com/images/media/meals/uuuspp1511297945.jpg",
+  },
+  {
+    id: 2,
+    title: "Christmas Cake",
+    description:
+      "Heat oven to 160C/fan 140C/gas 3. Line the base and sides of a 20 cm round, 7.5 cm deep cake tin. Beat the butter and sugar with an electric hand mixer for 1-2 mins until very creamy and pale in colour, scraping down the sides of the bowl half way through.",
+    image: "https://www.themealdb.com/images/media/meals/ldnrm91576791881.jpg",
+  },
+  {
+    id: 3,
+    title: "Minced Beef Pie ",
+    description:
+      "Preheat the oven to 200C/400F/Gas 6. Heat the oil in a deep frying pan and fry the beef mince for 4-5 minutes, breaking it up with a wooden spoon as it browns. Add the onion and cook for 2-3 minutes, then stir in the tomato purée and cook for 2-3 more minutes. Stir in the flour and cook for a further minute, then add the chopped mushrooms.",
+    image: "https://www.themealdb.com/images/media/meals/xwutvy1511555540.jpg",
+  },
+  {
+    id: 4,
+    title: "Kedgeree",
+    description:
+      "For the rice, heat the oil in a large, lidded pan, add the onion, then gently fry for 5 mins until softened but not coloured. Add the spices, season with salt, then continue to fry until the mix start to go brown and fragrant; about 3 mins. Add the rice and stir in well. Add 600ml water, stir, then bring to the boil.",
+    image: "https://www.themealdb.com/images/media/meals/utxqpt1511639216.jpg",
+  },
+  {
+    id: 5,
+    title: "Sticky Toffee Pudding",
+    description:
+      "Preheat the oven to 180C/160C Fan/Gas 4. Butter a wide shallow 1.7-litre/3-pint ovenproof dish. Put the butter, sugar, eggs, flour, baking powder, bicarbonate of soda and treacle into a mixing bowl.",
+    image: "https://www.themealdb.com/images/media/meals/xqqqtu1511637379.jpg",
+  },
+  {
+    id: 6,
+    title: "Toad In The Hole",
+    description:
+      "Preheat the oven to 200°C/fan180°C/gas 6. fry sausages in a non-stick pan until browned. Drizzle vegetable oil in a 30cm x 25cm x 6cm deep roasting tray and heat in the oven for 5 minutes. Put the plain flour in a bowl, crack in the medium free-range eggs.",
+    image: "https://www.themealdb.com/images/media/meals/ytuvwr1503070420.jpg",
+  },
+  {
+    id: 7,
+    title: "Beef Dumpling Stew",
+    description:
+      "Preheat the oven to 180C/350F/Gas 4. For the beef stew, heat the oil and butter in an ovenproof casserole and fry the beef until browned on all sides. Sprinkle over the flour and cook for a further 2-3 minutes. Add the garlic and all the vegetables and fry for 1-2 minutes. Stir in the wine, stock and herbs, then add the Worcestershire sauce and balsamic vinegar, to taste.",
+    image: "https://www.themealdb.com/images/media/meals/uyqrrv1511553350.jpg",
+  },
+  {
+    id: 8,
+    title: "Vegetarian Casserole",
+    description:
+      "Heat the oil in a large, heavy-based pan. Add the onions and cook gently for 5 – 10 mins until softened. Add the garlic, spices, dried thyme, carrots, celery and peppers and cook for 5 minutes.",
+    image: "https://www.themealdb.com/images/media/meals/vptwyt1511450962.jpg",
+  },
+  {
+    id: 9,
+    title: "Mince Pies",
+    description:
+      "To make the pastry, rub 225g cold, diced butter into 350g plain flour, then mix in 100g golden caster sugar and a pinch of salt. Combine the pastry into a ball – don’t add liquid – and knead it briefly. The dough will be fairly firm, like shortbread dough. You can use the dough immediately, or chill for later.",
+    image: "https://www.themealdb.com/images/media/meals/qe8pf51576795532.jpg",
+  },
+];
+
+export default recipes;
